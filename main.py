27277c60725185339f7f75e4ce4d42ecb36d3d52
Bartosz Słapa <useyour@email.com>
2021-05-28 16:10:45 +0200

    Added a file!

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ who = input("Oh, and who are you? Tell me: ")
 print(f"Well hello to you too {who}.")
 
 def fun_doing_nothing(with_this):
-    print("I've done nothing with {with_this}.")
+    print(f"I've done nothing with {with_this}.")
+
+
+fun_doing_nothing(who)
