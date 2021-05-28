81100f1ad3f465150c7fc285f6745460ed633afb
Bartosz Słapa <useyour@email.com>
2021-05-28 16:08:00 +0200

    Added a function.

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ print("Hello World!")
 
 who = input("Oh, and who are you? Tell me: ")
 print(f"Well hello to you too {who}.")
+
+def fun_doing_nothing(with_this):
+    print("I've done nothing with {with_this}.")
